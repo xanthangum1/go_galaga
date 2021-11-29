f94fe674a65e848c424156ccdd95ba90377c7e29
xanthangum1 <adriankim0722@gmail.com>
2021-11-29 23:05:40 +0900

    script not working

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/xanthangum1/go_galaga
+
+go 1.17
+
+require github.com/veandco/go-sdl2 v0.4.10 // indirect
---
+++ main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/veandco/go-sdl2/sdl"
+)
+
+const (
+	screenWidth  = 800
+	screenHeight = 1200
+)
+
+func main() {
+	if err := sdl.Init(sdl.INIT_EVERYTHING); err != nil {
+		fmt.Println("initializing SDL:", err)
+		return
+	}
+
+	window, err := sdl.CreateWindow(
+		"Gaming in Go",
+		sdl.WINDOWPOS_UNDEFINED, sdl.WINDOWPOS_UNDEFINED,
+		screenWidth, screenHeight,
+		sdl.WINDOW_OPENGL)
+	if err != nil {
+		fmt.Println("initializing window:", err)
+		return
+	}
+	defer window.Destroy()
+
+	renderer, err := sdl.CreateRenderer(window, -1, sdl.RENDERER_ACCELERATED)
+	if err != nil {
+		fmt.Println("initializing renderer:", err)
+		return
+	}
+	defer renderer.Destroy()
+
+	for {
+		renderer.SetDrawColor(255, 255, 255, 255)
+		renderer.Clear()
+
+		renderer.Present()
+	}
+}
