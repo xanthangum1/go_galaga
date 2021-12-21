359cd1616c4fe088ae3a76455149d38ea46be9d3
xanthangum1 <adriankim0722@gmail.com>
2021-12-21 13:39:44 +0900

    create animator file

--- animator.go
+++ animator.go
@@ -1,5 +1,15 @@
 package main
 
+import "github.com/veandco/go-sdl2/sdl"
+
 type animator struct {
 	containter *element
+	sequences  map[string]*sequence
+}
+
+type sequence struct {
+	textures []*sdl.Texture
+	frame    int
+	// rate of frame change per second
+	sampleRate float64
 }
